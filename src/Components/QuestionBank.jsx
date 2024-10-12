3fea82eea671e4e8edb3cc669187132373c92fb9
ddarko98 <darko.spasojevic33@gmail.com>
2024-10-12 19:36:13 +0200

    first commit

---
+++ src/Components/QuestionBank.jsx
@@ -0,0 +1,34 @@
+const qBank = [
+    {
+        id: 1,
+        question: "Koji filozof antike je tvrdio da je čovek mera svih stvari?",
+        options: ["Gorgija", "Protagora", "Aristotel", "Sokrat"],
+        answer: "Protagora",
+    },
+    {
+        id: 2,
+        question: "Koji rimski filozof je napisao delo 'Pisma prijatelju'?",
+        options: ["Ciceron", "Seneka", "Boteije", "Lukrecije"],
+        answer: "Seneka",
+    },
+    {
+        id: 3,
+        question: "Veliku duhovnu inspiraciju sv. Toma Akvinski pronalazi u kom filozofu?",
+        options: ["Aristotel", "Ciceron", "Heraklit", "Dekart"],
+        answer: "Aristotel"
+    },
+    {
+        id: 4,
+        question: "'Cogito ergo sum' je izgovorio koji filozof?",
+        options: ["Hjum", "Spinoza", "Dekart", "Lok"],
+        answer: "Dekart"
+    },
+    {
+        id: 5,
+        question: "'Sve što je umsko stvarno je, sve što je stvarno umsko je' je maksima kog filozofa?",
+        options: ["Kant", "Fihte", "Šeling", "Hegel"],
+        answer: "Lucknow"
+    },
+]
+
+export default qBank;
